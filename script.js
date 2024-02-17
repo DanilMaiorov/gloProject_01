e45f2cf0973c8c9a5f4e66e700f4c1b2da760237
damayorov93@gmail.com <damayorov93@gmail.com>
2024-02-17 12:55:18 +0500

    lesson_02_normal

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+const title = "Калькулятор"
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 1000;
+let rollback = 15;
+let fullPrice = 1500;
+let adaptive = true;
 
-alert('alert alert');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
-console.log('console.log');
+console.log(screens.length);
+
+console.log(`Стоимость верстки экранов ${screenPrice} рублей/долларов/гривен/юани`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей/долларов/гривен/юани`);
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани");
+
+let lowerCaseScreens = screens.toLowerCase().split(', ');
+console.log(lowerCaseScreens);
+
+console.log(fullPrice * (rollback / 100));
