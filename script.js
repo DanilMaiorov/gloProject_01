049b873d14c42db9bc80e3da273e271789644701
damayorov93@gmail.com <damayorov93@gmail.com>
2024-02-19 20:37:52 +0500

    lesson_03

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-const title = "Калькулятор"
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 1000;
+'use strict'
+/* const title = prompt("Как называется ваш проект?", "Калькулятор");
+let screens = prompt("Какие типы экранов нужно разработать?", "Простые"); */
+let screenPrice = +prompt("Сколько будет стоить данная работа?", 10000);
+
+/* let service1 = prompt("Какой дополнительный тип услуги нужен?", "Метрика"); */
+let servicePrice1 = +prompt("Сколько это будет стоить?", 2000);
+/* let service2 = prompt("Какой дополнительный тип услуги нужен?", "Попап"); */
+let servicePrice2 = +prompt("Сколько это будет стоить?", 1000);
+
+
+let fullPrice = calcFullPrice(screenPrice, servicePrice1, servicePrice2);
+
 let rollback = 15;
-let fullPrice = 1500;
-let adaptive = true;
+
+let servicePercentPrice = calcServicePercentPrice(fullPrice, rollback)
+
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+
+function calcFullPrice(arg1, arg2, arg3) {
+  let sum = arg1 + arg2 + arg3;
+  console.log(sum);
+  if (sum > 30000) {
+    console.log("Даем скидку в 10%");
+  } else if (sum < 30000 && sum >= 15000) {
+    console.log("Даем скидку в 5%");
+  } else if (sum < 15000 && sum > 0) {
+    console.log("Скидка не предусмотрена");
+  } else {
+    console.log("Что то пошло не так");
+  }
+  return sum;
+}
+
+function calcServicePercentPrice(fullPrice, rollback) {
+  return Math.round((fullPrice - (fullPrice * (rollback / 100))) / 100) * 100;
+}
+
+
+console.log(fullPrice);
+console.log(servicePercentPrice);
 
 console.log(typeof title);
 console.log(typeof fullPrice);
